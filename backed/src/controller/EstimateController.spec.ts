f4cee8eec3e51d95ccd77baf5387e776c7992036
Tiago Gomes <tidgomes@gmail.com>
2024-11-21 19:00:29 -0300

    refactor: remove the fetch call from the EstimateController

--- backed/src/controller/EstimateController.spec.ts
+++ backed/src/controller/EstimateController.spec.ts
@@ -1,34 +1,15 @@
+import { FetchWrapper } from "../utils/FetchWrapper"
 import { EstimateController } from "./EstimateController"
-import 'dotenv/config'
 
-const routesApiDummyReturn = {
-    "routes": [
-        {
-            "legs": [
-                {
-                    "startLocation": {
-                        "latLng": {
-                            "latitude": 37.4227804,
-                            "longitude": -122.08412609999999
-                        }
-                    },
-                    "endLocation": {
-                        "latLng": {
-                            "latitude": 37.7353976,
-                            "longitude": -122.50666500000001
-                        }
-                    }
-                }
-            ],
-            "distanceMeters": 57498,
-            "duration": "2403s"
-        }
-    ]
+const makeSut = (): EstimateController => {
+    const fetchWrapper = new FetchWrapper();
+    const sut = new EstimateController(fetchWrapper)
+    return sut
 }
 
 describe('EstimateController', () => {
     test('should return 400 if no origin is provided', async () => {
-        const sut = new EstimateController()
+        const sut = makeSut()
 
         const httpRequest = {
             'body': {
@@ -47,7 +28,7 @@ describe('EstimateController', () => {
     })
 
     test('should return 400 if no destination is provided', async () => {
-        const sut = new EstimateController()
+        const sut = makeSut()
 
         const httpRequest = {
             'body': {
@@ -66,7 +47,7 @@ describe('EstimateController', () => {
     })
 
     test('should return 400 if no customer_id is provided', async () => {
-        const sut = new EstimateController()
+        const sut = makeSut()
 
         const httpRequest = {
             'body': {
@@ -85,7 +66,7 @@ describe('EstimateController', () => {
     })
 
     test('should return 400 if origin address is equal to the destination address', async () => {
-        const sut = new EstimateController()
+        const sut = makeSut()
 
         const httpRequest = {
             'body': {
@@ -105,7 +86,7 @@ describe('EstimateController', () => {
     })
 
     test('should return 400 if customer_id, origin or destination is a string of blank spaces', async () => {
-        const sut = new EstimateController()
+        const sut = makeSut()
 
         const httpRequest = {
             'body': {
@@ -121,29 +102,5 @@ describe('EstimateController', () => {
             "error_code": "INVALID_DATA",
             "error_description": "Os dados fornecidos no corpo da requisição são inválidos"
         })
-    })
-
-    test('should call fetch API with correct parameters', async () => {
-        const sut = new EstimateController()
-
-        const httpRequest = {
-            body: {
-                'customer_id': 'any_id',
-                'origin': 'any_origin',
-                'destination': 'any_destination'
-            }
-        }
-        const url = 'https://routes.googleapis.com/directions/v2:computeRoutes'
-        const method = 'POST'
-        const headers = {
-            'Content-Type': 'application/json',
-            'X-Goog-Api-Key': process.env.GOOGLE_API_KEY,
-            'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.legs.startLocation,routes.legs.endLocation'
-        }
-
-        const fetchSpy = jest.spyOn(global, 'fetch')
-        await sut.handle(httpRequest)
-
-        expect(fetchSpy).toHaveBeenCalledWith(url, { method, headers })
     })
 })
--- backed/src/controller/EstimateController.ts
+++ backed/src/controller/EstimateController.ts
@@ -1,8 +1,16 @@
 import { badRequest } from "../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import 'dotenv/config'
+import { FetchWrapper } from "../utils/FetchWrapper";
 
 export class EstimateController {
+
+    private readonly fetchWrapper: FetchWrapper
+
+    constructor(fetchWrapper: FetchWrapper) {
+        this.fetchWrapper = fetchWrapper
+    }
+
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const requiredFields = ['customer_id', 'origin', 'destination']
         for (const field of requiredFields) {
@@ -12,13 +20,9 @@ export class EstimateController {
 
         if (httpRequest.body.origin === httpRequest.body.destination) return badRequest()
 
-        const routesApiResponse = await fetch('https://routes.googleapis.com/directions/v2:computeRoutes',
-            { method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Goog-Api-Key': process.env.GOOGLE_API_KEY,
-                'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.legs.startLocation,routes.legs.endLocation'}
-            }
-        )
+        const originAddress = httpRequest.body.origin
+        const destinationAddress = httpRequest.body.destination
+
+        const routesApiResponse = this.fetchWrapper.fetchFromRoutesApi(originAddress, destinationAddress)
     }
 }
