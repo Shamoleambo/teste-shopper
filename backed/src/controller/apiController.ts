dd3dcffd3c84dca18103c3951abfd608ed579e2b
Tiago Gomes <tidgomes@gmail.com>
2024-11-21 13:20:23 -0300

    feat: ensure api controller returns a body in case the data are invalid

--- backed/src/controller/apiController.ts
+++ backed/src/controller/apiController.ts
@@ -1,7 +1,11 @@
 export class ApiController {
     handle(httpRequest: any): any {
         return {
-            statusCode: 400
+            statusCode: 400,
+            body: {
+                "error_code": "INVALID_DATA",
+                "error_description": "any_description"
+            }
         }
     }
 }
