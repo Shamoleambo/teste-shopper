a3d8390f2d4c96a66229a08fb707f629e1dabf42
Tiago Gomes <tidgomes@gmail.com>
2024-11-21 17:39:09 -0300

    feat: call fetch api to retriece data from Routes API

--- backed/src/controller/EstimateController.ts
+++ backed/src/controller/EstimateController.ts
@@ -1,8 +1,9 @@
 import { badRequest } from "../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols/http";
+import 'dotenv/config'
 
 export class EstimateController {
-    handle(httpRequest: HttpRequest): HttpResponse {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const requiredFields = ['customer_id', 'origin', 'destination']
         for (const field of requiredFields) {
             if (!httpRequest.body[field]) return badRequest()
@@ -10,5 +11,14 @@ export class EstimateController {
         }
 
         if (httpRequest.body.origin === httpRequest.body.destination) return badRequest()
+
+        const routesApiResponse = await fetch('https://routes.googleapis.com/directions/v2:computeRoutes',
+            { method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Goog-Api-Key': process.env.GOOGLE_API_KEY,
+                'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.legs.startLocation,routes.legs.endLocation'}
+            }
+        )
     }
 }
