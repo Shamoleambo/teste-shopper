cf7e1e6a3d2b95b8c7e1bdd582c142f538102665
Tiago Gomes <tidgomes@gmail.com>
2024-11-22 13:52:27 -0300

    feat: validate if there is a driver field in the request

--- backed/src/controller/ConfirmController.ts
+++ backed/src/controller/ConfirmController.ts
@@ -4,7 +4,7 @@ import { Controller } from "./IController";
 
 export class ConfirmController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const requiredFields = ['customer_id', 'origin', 'destination', 'distance', 'duration', 'driver']
+        const requiredFields = ['customer_id', 'origin', 'destination', 'distance', 'duration', 'driver', 'value']
         for(const field of requiredFields) {
             if(!httpRequest.body[field]) return badRequest()
         }
