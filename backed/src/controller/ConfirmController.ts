a5db885f40afa1d264a9fc55925396794d475f27
Tiago Gomes <tidgomes@gmail.com>
2024-11-22 13:43:33 -0300

    feat: validate if there is a customer_id field in the request

---
+++ backed/src/controller/ConfirmController.ts
@@ -0,0 +1,14 @@
+import { badRequest } from "../helpers/http-helper";
+import { HttpRequest, HttpResponse } from "../protocols/http";
+import { Controller } from "./IController";
+
+export class ConfirmController implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const requiredFields = ['customer_id']
+        for(const field of requiredFields) {
+            if(!httpRequest.body[field]) return badRequest()
+        }
+
+        return null
+    }
+}
