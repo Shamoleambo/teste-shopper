feff0e4885eda0a1e23cbebeb153aba41c762754
Tiago Gomes <tidgomes@gmail.com>
2024-11-22 12:12:13 -0300

    feat: configure body parser and cors

--- backed/src/main/server.ts
+++ backed/src/main/server.ts
@@ -1,5 +1,14 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
+import { json } from "express"
 
 const app = express()
+
+app.use(json())
+app.use((req: Request, res: Response, next: NextFunction): void => {
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-headers', '*')
+    res.set('access-control-allow-methods', '*')
+    next()
+})
 
 app.listen(8080, () => console.log('Server running at http://localhost:8080'))
