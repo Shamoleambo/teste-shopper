c805d4b9ccc5a4be7c0dbaa332f61658cec67055
Tiago Gomes <tidgomes@gmail.com>
2024-11-22 13:45:48 -0300

    test: validate if there is an origin field in the request

--- backed/src/controller/ConfirmController.spec.ts
+++ backed/src/controller/ConfirmController.spec.ts
@@ -11,12 +11,37 @@ const makeSut = (): SutTypes => {
 }
 
 describe('ConfirmController', () => {
-    test('should return 400 if there is no customer_id', async () => {
+    test('should return 400 if there is no customer_id in the request', async () => {
         const { sut } = makeSut()
 
         const httpRequest = {
             body: {
                 origin: "any_origin",
+                destination: "any_destination",
+                distance: 100,
+                duration: "any_duration",
+                driver: {
+                    id: 123,
+                    name: "any_name"
+                },
+                value: 123
+            }
+        }
+        const httpResponse = await sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual({
+            "error_code": "INVALID_DATA",
+            "error_description": "Os dados fornecidos no corpo da requisição são inválidos"
+        })
+    })
+
+    test('should return 400 if there is no origin in the request', async () => {
+        const { sut } = makeSut()
+
+        const httpRequest = {
+            body: {
+                customer_id: '123',
                 destination: "any_destination",
                 distance: 100,
                 duration: "any_duration",
