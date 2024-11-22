d985608a1afa405c2c92658008ae5f8e76e156d8
Tiago Gomes <tidgomes@gmail.com>
2024-11-22 13:51:20 -0300

    test: validate if there is a driver field in the request

--- backed/src/controller/ConfirmController.spec.ts
+++ backed/src/controller/ConfirmController.spec.ts
@@ -135,4 +135,26 @@ describe('ConfirmController', () => {
             "error_description": "Os dados fornecidos no corpo da requisição são inválidos"
         })
     })
+
+    test('should return 400 if there is no driver in the request', async () => {
+        const { sut } = makeSut()
+
+        const httpRequest = {
+            body: {
+                customer_id: '123',
+                origin: 'any_origin',
+                destination: 'any_destination',
+                distance: 100,
+                duration: 'any_duration',
+                value: 123
+            }
+        }
+        const httpResponse = await sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual({
+            "error_code": "INVALID_DATA",
+            "error_description": "Os dados fornecidos no corpo da requisição são inválidos"
+        })
+    })
 })
