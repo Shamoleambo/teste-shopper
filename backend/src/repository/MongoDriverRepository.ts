2f0c3e6e6ecdcda542c496db02cb05ad386cd118
Tiago Gomes <tidgomes@gmail.com>
2024-11-23 17:41:18 -0300

    refactor: remove unnecessary logs

--- backend/src/repository/MongoDriverRepository.ts
+++ backend/src/repository/MongoDriverRepository.ts
@@ -5,10 +5,7 @@ import { DriverRepository } from "./DriverRepository";
 export class MongoDriverRepository implements DriverRepository {
 
     async getDrivers(): Promise<Driver[]> {
-        console.log('\n\ndentro do MongoDriverRepository\n\n')
         const drivers = await MongoClient.db.collection<Driver>('drivers').find({}).toArray()
-
-        console.log(drivers)
 
         return drivers
     }
