6465a2915aa42f7bc2f2ac2cc73422758ef678ad
Tiago Gomes <tidgomes@gmail.com>
2024-11-24 16:00:10 -0300

    refactor: preset the id for the driver

--- mongo-seed/seed.js
+++ mongo-seed/seed.js
@@ -1,6 +1,7 @@
 db = db.getSiblingDB("taxi")
 db.drivers.insertMany([
     {
+        _id: "1",
         name: "Homer Simpson",
         description: "Olá! Sou o Homer, seu motorista camarada! Relaxe e aproveite o passeio, com direito a rosquinhas e boas risadas (e talvez alguns desvios).",
         vehicle: "Plymouth Valiant 1973 rosa e enferrujado",
@@ -12,6 +13,7 @@ db.drivers.insertMany([
         minimumDistance: 1
     },
     {
+        _id: "2",
         name: "Dominic Toretto",
         description: "Ei, aqui é o Dom. Pode entrar, vou te levar com segurança e rapidez ao seu destino. Só não mexa no rádio, a playlist é sagrada.",
         vehicle: "Dodge Charger R/T 1970 modificado",
@@ -23,6 +25,7 @@ db.drivers.insertMany([
         minimumDistance: 5
     },
     {
+        _id: "3",
         name: "James Bond",
         description: "Boa noite, sou James Bond. À seu dispor para um passeio suave e discreto. Aperte o cinto e aproveite a viagem.",
         vehicle: "Aston Martin DB5 clássico",
