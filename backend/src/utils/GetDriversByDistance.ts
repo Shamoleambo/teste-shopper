9e04ffa6260e1ed7693619c73527132b1d7ed13f
Tiago Gomes <tidgomes@gmail.com>
2024-11-24 15:21:47 -0300

    feat: return drivers with ride value

--- backend/src/utils/GetDriversByDistance.ts
+++ backend/src/utils/GetDriversByDistance.ts
@@ -1,15 +1,26 @@
+import { ResponseDriver } from "../protocols/ResponseDrivers";
 import { DriverRepository } from "../repository/DriverRepository";
+import { TaxiRide } from "./TaxiRide";
 
 export class GetDriversByDistance {
-    constructor(private readonly mongoDriversRepository: DriverRepository) {
+    constructor(private readonly mongoDriversRepository: DriverRepository, private readonly ride: TaxiRide) {
         this.mongoDriversRepository = mongoDriversRepository
+        this.ride = ride
     }
 
-    async check(distance: number) {
+    async check(distance: number): Promise<ResponseDriver[]> {
 
         const drivers = await this.mongoDriversRepository.getDrivers()
         const filteredDrivers = drivers.filter(driver => distance >= driver.minimumDistance * 1000)
 
-        return filteredDrivers
+        const responseDrivers = filteredDrivers.map(d => {
+            const value = this.ride.calculateValue(d, distance)
+            const driver = { ...d, value }
+            delete driver.ratio
+            delete driver.minimumDistance
+            return driver
+        })
+
+        return responseDrivers
     }
 }
