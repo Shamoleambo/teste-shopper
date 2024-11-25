04773772e531583bef9ed5cd5a3e596d32291c4d
Tiago Gomes <tidgomes@gmail.com>
2024-11-25 18:18:44 -0300

    test: add another expected scenario for the db fetch

--- backend/src/controller/RideController.spec.ts
+++ backend/src/controller/RideController.spec.ts
@@ -4,15 +4,17 @@ import { RideController } from "./RideController"
 type SutTypes = {
     sut: Controller
 }
+    driverRepository: DriverRepository
 
 const makeSut = (): SutTypes => {
-    const sut = new RideController()
-    return { sut }
+    const driverRepository = new MongoDriverRepository()
+    const sut = new RideController(driverRepository)
+    return { sut, driverRepository }
 }
 
 describe('RideController', () => {
     test('should return 400 if driver is invalid', async () => {
-        const { sut } = makeSut()
+        const { sut, driverRepository } = makeSut()
 
         const httpRequest = {
             params: {
@@ -21,8 +23,10 @@ describe('RideController', () => {
             }
         }
 
+        const getDriverByIdSpy = jest.spyOn(driverRepository, 'getDriverById').mockResolvedValueOnce(null)
         const httpResponse = await sut.handle(httpRequest)
 
+        expect(getDriverByIdSpy).toHaveBeenCalledTimes(1)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual({
             error_code: "INVALID_DRIVER",
