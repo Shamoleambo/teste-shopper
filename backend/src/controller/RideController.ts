0bb090c2ef96bc35e24014da47e6ffa6cae1817c
Tiago Gomes <tidgomes@gmail.com>
2024-11-25 18:17:51 -0300

    feat: add another expected scenario for the db fetch

--- backend/src/controller/RideController.ts
+++ backend/src/controller/RideController.ts
@@ -1,10 +1,20 @@
 import { invalidDriver } from "../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols/http";
+import { DriverRepository } from "../repository/DriverRepository";
 import { Controller } from "./Controller";
 
 export class RideController implements Controller {
 
+    private readonly driverRepository: DriverRepository
+
+    constructor(driverRepository: DriverRepository) {
+        this.driverRepository = driverRepository
+    }
+
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        return invalidDriver()
+        const driver = await this.driverRepository.getDriverById(httpRequest.params.driver_id)
+        if(!driver) return invalidDriver()
+        
+        return null
     }
 }
