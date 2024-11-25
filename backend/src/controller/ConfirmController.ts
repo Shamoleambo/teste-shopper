07dbb85da75983eca6e327921f57c7eafb8a3f0e
Tiago Gomes <tidgomes@gmail.com>
2024-11-25 15:58:30 -0300

    feat: save the customer with the ride

--- backend/src/controller/ConfirmController.ts
+++ backend/src/controller/ConfirmController.ts
@@ -1,14 +1,18 @@
+import { ObjectId } from "mongodb";
 import { badRequest, distanceNotValidForDriver, driverNotFound } from "../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols/http";
+import { CustomerRepository } from "../repository/CustomerRepository";
 import { DriverRepository } from "../repository/DriverRepository";
 import { Controller } from "./Controller";
 
 export class ConfirmController implements Controller {
 
     private readonly driverRepository: DriverRepository
+    private readonly customerRepository: CustomerRepository
 
-    constructor(driverRepository: DriverRepository) {
+    constructor(driverRepository: DriverRepository, customerRepository) {
         this.driverRepository = driverRepository
+        this.customerRepository = customerRepository
     }
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -22,10 +26,28 @@ export class ConfirmController implements Controller {
 
         if (!httpRequest.body.driver.name.trim()) return badRequest()
 
-        const driver = await this.driverRepository.getDriverById(httpRequest.body.driver._id)
+        const driver = await this.driverRepository.getDriverById(httpRequest.body.driver.id)
         if (!driver) return driverNotFound()
 
         if (driver.minimumDistance * 1000 > httpRequest.body.distance) return distanceNotValidForDriver()
+
+        const customerId = httpRequest.body.customer_id
+        const ride = {
+            id: Math.floor(Math.random() * 10000),
+            "customer_id": httpRequest.body.customer_id,
+            date: new Date(),
+            origin: httpRequest.body.origin,
+            destination: httpRequest.body.destination,
+            distance: httpRequest.body.distance,
+            duration: httpRequest.body.duration,
+            driver: {
+                id: httpRequest.body.driver.id,
+                name: httpRequest.body.driver.name
+            },
+            value: httpRequest.body.value
+        }
+
+        await this.customerRepository.saveCustomer(customerId, ride)
 
         return {
             statusCode: 200,
