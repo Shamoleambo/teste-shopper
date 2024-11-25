216c905caf0abe3497a2f97c4873c998a81345d9
Tiago Gomes <tidgomes@gmail.com>
2024-11-25 16:00:45 -0300

    feat: add new route to the application

---
+++ backend/src/main/ConfirmControllerFactory.ts
@@ -0,0 +1,10 @@
+import { ConfirmController } from "../controller/ConfirmController";
+import { MongoCustomerRepository } from "../repository/MongoCustomerRepository";
+import { MongoDriverRepository } from "../repository/MongoDriverRepository";
+
+export const makeConfirmController = (): ConfirmController => {
+    const driverRepository = new MongoDriverRepository()
+    const customerRepository = new MongoCustomerRepository()
+    const confirmController = new ConfirmController(driverRepository, customerRepository)
+    return confirmController
+}
--- backend/src/main/routes.ts
+++ backend/src/main/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { adaptRoute } from "./controllerAdapter";
 import { makeEstimateController } from "./EstimateControllerFactory";
+import { makeConfirmController } from "./ConfirmControllerFactory";
 
 const router = Router()
 
 router.post('/ride/estimate', adaptRoute(makeEstimateController()))
+router.patch('/ride/confirm', adaptRoute(makeConfirmController()))
 
 export default router
