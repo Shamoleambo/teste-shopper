64589184b23d93a6b1b408efd091cace36ea9237
Tiago Gomes <tidgomes@gmail.com>
2024-11-26 14:13:49 -0300

    feat: create basic form structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,28 @@ import './App.css'
 
 function App() {
 
+  const submitHandler = (event: React.FormEvent): void => {
+    event.preventDefault()
+  }
+
   return (
-    <>
-      <h1>Hello Simple React With TS APP!</h1>
-    </>
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className='input-wrapper'>
+          <label htmlFor='userId'>User Id:</label>
+          <input type='number' id='userId' />
+        </div>
+        <div className='input-wrapper'>
+          <label htmlFor='originAddress'>Origin Address:</label>
+          <input type='text' id='originAddress' />
+        </div>
+        <div className='input-wrapper'>
+          <label htmlFor='destinationAddress'>Destination Address:</label>
+          <input type='text' id='destinationAddress' />
+        </div>
+        <button type='submit'>Estimate Ride</button>
+      </form>
+    </div>
   )
 }
 
