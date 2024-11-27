9bc0fc48573108f433b3286e7db66ad0c9b16566
Tiago Gomes <tidgomes@gmail.com>
2024-11-27 18:12:52 -0300

    refactor: change the form to store the state differently

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,11 +1,17 @@
-import { useState, FormEvent } from 'react'
+import { useRef, FormEvent } from 'react'
 import { RideAction } from '../reducers/rideTypes'
 
 
-const Form: React.FC<{ dispatchRide: (action: RideAction) => void }> = (props) => {
-    const [customerId, setCustomerId] = useState("")
-    const [origin, setOrigin] = useState("")
-    const [destination, setDestination] = useState("")
+const Form: React.FC<{
+    dispatchRide: (action: RideAction) => void,
+    setCustomerId: (id: string) => void,
+    setOrigin: (origin: string) => void,
+    setDestination: (destination: string) => void
+}> = (props) => {
+
+    const costumerIdInput = useRef<HTMLInputElement>(null)
+    const originInput = useRef<HTMLInputElement>(null)
+    const destinationInput = useRef<HTMLInputElement>(null)
 
 
     const submitHandler = async (event: React.FormEvent): Promise<void> => {
@@ -14,9 +20,9 @@ const Form: React.FC<{ dispatchRide: (action: RideAction) => void }> = (props) =
         const response = await fetch('http://localhost:8080/ride/estimate', {
             method: 'POST',
             body: JSON.stringify({
-                customer_id: customerId,
-                origin: origin,
-                destination: destination
+                customer_id: costumerIdInput.current?.value,
+                origin: originInput.current?.value,
+                destination: destinationInput.current?.value
             }),
             headers: {
                 'Content-Type': 'application/json'
@@ -24,10 +30,11 @@ const Form: React.FC<{ dispatchRide: (action: RideAction) => void }> = (props) =
         })
 
         const data = await response.json()
+        console.log(data)
 
-        props.dispatchRide({ type: 'SET_CUSTOMER_ID', payload: customerId })
-        props.dispatchRide({ type: 'SET_ORIGIN_ADDRESS', payload: origin })
-        props.dispatchRide({ type: 'SET_DESTINATION_ADDRESS', payload: destination })
+        // props.dispatchRide({ type: 'SET_CUSTOMER_ID', payload: customerId })
+        // props.dispatchRide({ type: 'SET_ORIGIN_ADDRESS', payload: origin })
+        // props.dispatchRide({ type: 'SET_DESTINATION_ADDRESS', payload: destination })
         props.dispatchRide({ type: 'SET_ORIGIN', payload: { lat: data.origin.latitude, long: data.origin.longitude } })
         props.dispatchRide({ type: 'SET_DESTINATION', payload: { lat: data.destination.latitude, long: data.destination.longitude } })
         props.dispatchRide({ type: 'SET_DISTANCE', payload: data.distance })
@@ -42,7 +49,8 @@ const Form: React.FC<{ dispatchRide: (action: RideAction) => void }> = (props) =
             <input
                 type='number'
                 id='userId'
-                onChange={(e: FormEvent<HTMLInputElement>) => setCustomerId(e.currentTarget.value)}
+                ref={costumerIdInput}
+                onChange={(e: FormEvent<HTMLInputElement>) => props.setCustomerId(e.currentTarget.value)}
             />
         </div>
         <div className='input-wrapper'>
@@ -50,7 +58,8 @@ const Form: React.FC<{ dispatchRide: (action: RideAction) => void }> = (props) =
             <input
                 type='text'
                 id='originAddress'
-                onChange={(e: FormEvent<HTMLInputElement>) => setOrigin(e.currentTarget.value)}
+                ref={originInput}
+                onChange={(e: FormEvent<HTMLInputElement>) => props.setOrigin(e.currentTarget.value)}
             />
         </div>
         <div className='input-wrapper'>
@@ -58,7 +67,8 @@ const Form: React.FC<{ dispatchRide: (action: RideAction) => void }> = (props) =
             <input
                 type='text'
                 id='destinationAddress'
-                onChange={(e: FormEvent<HTMLInputElement>) => setDestination(e.currentTarget.value)}
+                ref={destinationInput}
+                onChange={(e: FormEvent<HTMLInputElement>) => props.setDestination(e.currentTarget.value)}
             />
         </div>
         <button type='submit'>Estimate Ride</button>
