20bf116a303ed2dc95ba7e474834a3343b57dc8e
Tiago Gomes <tidgomes@gmail.com>
2024-11-27 08:52:51 -0300

    feat: create submitHandler function for the form element

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
+import { useRef } from 'react'
 import './App.css'
 
 function App() {
+  console.log('meu caro amigo')
 
-  const submitHandler = (event: React.FormEvent): void => {
+  const userIdInputRef = useRef<HTMLInputElement>(null)
+  const originInputRef = useRef<HTMLInputElement>(null)
+  const destinationInputRef = useRef<HTMLInputElement>(null)
+
+  const submitHandler = async (event: React.FormEvent): Promise<void> => {
     event.preventDefault()
+
+    const response = await fetch('http://localhost:8080/ride/estimate', {
+      method: 'POST',
+      body: JSON.stringify({
+        customer_id: userIdInputRef.current?.value,
+        origin: originInputRef.current?.value,
+        destination: destinationInputRef.current?.value
+      })
+    })
+
+    const data = await response.json()
+    console.log(data)
+
   }
 
   return (
@@ -11,15 +30,15 @@ function App() {
       <form onSubmit={submitHandler}>
         <div className='input-wrapper'>
           <label htmlFor='userId'>User Id:</label>
-          <input type='number' id='userId' />
+          <input type='number' id='userId' ref={userIdInputRef} />
         </div>
         <div className='input-wrapper'>
           <label htmlFor='originAddress'>Origin Address:</label>
-          <input type='text' id='originAddress' />
+          <input type='text' id='originAddress' ref={originInputRef} />
         </div>
         <div className='input-wrapper'>
           <label htmlFor='destinationAddress'>Destination Address:</label>
-          <input type='text' id='destinationAddress' />
+          <input type='text' id='destinationAddress' ref={destinationInputRef} />
         </div>
         <button type='submit'>Estimate Ride</button>
       </form>
